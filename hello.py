032b1053e3ab97600b5b30bdd5dfeda1efda346e
Cycladis <o2k4ever@gmail.com>
2024-08-23 13:20:29 +0100

    Python lesson for the day

---
+++ hello.py
@@ -0,0 +1,17 @@
+print ('hello wiase')
+print ("hello wiase")
+
+quickmath = (4 + 5)
+print(quickmath)
+
+quickmath = (8*3)
+print(quickmath)
+
+quickmath = (4/3)
+print(quickmath)
+
+quickmath = (4.0 / 3)
+print(quickmath)
+
+quickmath = (3 + 5 * 9)
+print(quickmath)
